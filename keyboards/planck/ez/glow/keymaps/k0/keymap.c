a8645d599a838d9d8b16e5932a50753f02d17785
Qiantan Hong <qhong@alum.mit.edu>
2022-10-29 14:21:32 -0700

    update

--- keyboards/planck/ez/glow/keymaps/k0/keymap.c
+++ keyboards/planck/ez/glow/keymaps/k0/keymap.c
@@ -67,10 +67,10 @@ enum planck_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_planck_grid(
-    KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPACE,      
+    KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_SCOLON,
     KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_LPRN,        KC_QUOTE,       
-    KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_MINUS,       KC_SCOLON,      
-    LOWER,          KC_TRANSPARENT, KC_TRANSPARENT, KC_LGUI,        KC_LALT,        KC_SPACE,       KC_NO,          KC_LCTRL,       KC_RGUI,        KC_TRANSPARENT, KC_TRANSPARENT, RAISE
+    KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_MINUS,       KC_TRANSPARENT,
+    LOWER,          KC_TRANSPARENT, KC_TRANSPARENT, KC_LGUI,        KC_LALT,        MT(MOD_LSFT, KC_SPACE), KC_NO,  KC_LCTRL,       KC_RGUI,        KC_TRANSPARENT, KC_TRANSPARENT, RAISE
   ),
 
   [_LOWER] = LAYOUT_planck_grid(
@@ -96,10 +96,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-const key_override_t paren_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_SCOLON);
+const key_override_t paren_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_COLON);
+const key_override_t enter_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_M, KC_ENTER);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
   &paren_key_override,
+  &enter_key_override,
   NULL // Null terminate the array of overrides!
 };
 
