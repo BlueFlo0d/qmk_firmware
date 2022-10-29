9f7e7cfc0c58a082001129d919718a9c73ca1707
Qiantan Hong <qhong@alum.mit.edu>
2022-10-29 14:01:34 -0700

    key override

--- keyboards/planck/ez/glow/keymaps/k0/keymap.c
+++ keyboards/planck/ez/glow/keymaps/k0/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPACE,      
     KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_LPRN,        KC_QUOTE,       
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_MINUS,       KC_SCOLON,      
-    LOWER,          KC_TRANSPARENT, KC_TRANSPARENT, MT(MOD_LALT, KC_LGUI),KC_LALT,        KC_SPACE,       KC_NO,          KC_LCTRL,       KC_RGUI,        KC_TRANSPARENT, KC_TRANSPARENT, RAISE
+    LOWER,          KC_TRANSPARENT, KC_TRANSPARENT, KC_LGUI,        KC_LALT,        KC_SPACE,       KC_NO,          KC_LCTRL,       KC_RGUI,        KC_TRANSPARENT, KC_TRANSPARENT, RAISE
   ),
 
   [_LOWER] = LAYOUT_planck_grid(
@@ -94,6 +94,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_NO,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
 
+};
+
+const key_override_t paren_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_SCOLON);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+  &paren_key_override,
+  NULL // Null terminate the array of overrides!
 };
 
 extern rgb_config_t rgb_matrix_config;
