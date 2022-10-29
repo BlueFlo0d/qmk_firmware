8a6e9700253814f09ba0a93178b139af087a1b87
Qiantan Hong <qhong@alum.mit.edu>
2022-10-29 14:49:38 -0700

    fix

--- keyboards/planck/ez/glow/keymaps/k0/keymap.c
+++ keyboards/planck/ez/glow/keymaps/k0/keymap.c
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const key_override_t paren_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_COLON);
-const key_override_t enter_key_override = ko_make_basic(MOD_MASK_CONTROL, KC_M, KC_ENTER);
+const key_override_t enter_key_override = ko_make_basic(MOD_MASK_CTRL, KC_M, KC_ENTER);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
   &paren_key_override,
