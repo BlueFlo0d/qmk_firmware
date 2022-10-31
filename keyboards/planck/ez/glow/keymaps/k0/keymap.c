c5e0a3c7588b334931dab9f2f60a354dc955dd1b
Qiantan Hong <qhong@alum.mit.edu>
2022-10-30 19:50:58 -0700

    update

--- keyboards/planck/ez/glow/keymaps/k0/keymap.c
+++ keyboards/planck/ez/glow/keymaps/k0/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESCAPE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_SCOLON,
     KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_H,           KC_J,           KC_K,           KC_L,           KC_LPRN,        KC_QUOTE,       
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_MINUS,       KC_TRANSPARENT,
-    LOWER,          KC_TRANSPARENT, KC_TRANSPARENT, KC_LGUI,        KC_LALT,        MT(MOD_LSFT, KC_SPACE), KC_NO,  KC_LCTRL,       KC_RGUI,        KC_TRANSPARENT, KC_TRANSPARENT, RAISE
+    LOWER,          KC_TRANSPARENT, KC_TRANSPARENT, KC_LGUI,        KC_LALT,        MT(MOD_LSFT, KC_SPACE), KC_NO,  KC_LCTRL,       KC_RGUI,        KC_F1,          KC_TRANSPARENT, RAISE
   ),
 
   [_LOWER] = LAYOUT_planck_grid(
